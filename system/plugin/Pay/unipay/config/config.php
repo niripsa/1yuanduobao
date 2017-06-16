4f0e6c771dce2ede621afeba2c7c3cbfa88e77e7
linglong1987 <minds_education@163.com>
2017-06-16 17:02:46 +0800

    add wxjspay

--- system/plugin/Pay/unipay/config/config.php
+++ system/plugin/Pay/unipay/config/config.php
@@ -4,7 +4,9 @@ class Config{
         'url'=>'https://pay.swiftpass.cn/pay/gateway',
         'mchId'=>'102551501051',
         'key'=>'fc01c573641bc228b030f4ddefd5c621',
-        'version'=>'1.0'
+        'version'=>'1.0',
+        'appid' => 'wxdd992cf078094dbf',
+        'appsecret' => '1cec390c874a97c1c12d777dde2d1456',
     );
     
     public function C($cfgName){

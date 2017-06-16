6c215b4ffad411b7309d4407bcb6aa3bf24326c1
linglong1987 <minds_education@163.com>
2017-06-16 19:54:10 +0800

    add files

--- system/plugin/Pay/unipay/config/config.php
+++ system/plugin/Pay/unipay/config/config.php
@@ -2,8 +2,8 @@
 class Config{
     private $cfg = array(
         'url'=>'https://pay.swiftpass.cn/pay/gateway',
-        'mchId'=>'102551501051',
-        'key'=>'fc01c573641bc228b030f4ddefd5c621',
+        'mchId'=>'102581496021',
+        'key'=>'92737094324a314c043f1538b60ced34',
         'version'=>'1.0',
         'appid' => 'wxdd992cf078094dbf',
         'appsecret' => '1cec390c874a97c1c12d777dde2d1456',

a8dd9bba56e5c3757068a9307ec5da18672e16c1
linglong1987 <minds_education@163.com>
2017-06-19 22:36:23 +0800

    add admin game management

---
+++ system/modules/iinsist/lottery.action.php
@@ -0,0 +1,19 @@
+<?php
+defined("G_IN_SYSTEM") || exit("no");
+System::load_app_class("admin", G_ADMIN_DIR, "no");
+class lottery extends admin{
+    public function kj_record(){
+        //status=2 代表已开奖 status = 1代表还未开奖
+        $sql = "select * from `@#_lottery_stage` where `status` in (1,2) order by `end_time` desc limit 500";
+        $mysql_model = System::load_sys_class("model");
+        $aLotteryLists = $mysql_model->GetList($sql);
+        
+        $this->view->data('lottery_lists', $aLotteryLists);
+        $this->view->show("user.lottery_kj_record");
+    }
+
+    public function buy_record(){
+
+    }
+}
+?>

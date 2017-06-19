13897dfeb16f9e8bfbf4b46517e83a8b4069ee01
linglong1987 <minds_education@163.com>
2017-06-19 18:42:25 +0800

    add tpshop

--- tpshop/application/database.php
+++ tpshop/application/database.php
@@ -15,7 +15,7 @@ return [
     // 服务器地址
     'hostname'       => 'localhost',
     // 数据库名
-    'database'       => 'duobao',
+    'database'       => 'tpshop',
     // 用户名
     'username'       => 'duobao',
     // 密码
--- tpshop/install/templates/footer.php
+++
@@ -1,1 +0,0 @@
-<div class="footer"> &copy; 2014-2016 <a href="http://www.tp-shop.cn" target="_blank">www.tp-shop.cn</a> 深圳搜豹网络出品</div>
